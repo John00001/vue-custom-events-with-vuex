816edea1f206c0e8957eed0044b2b0d91705b3a8
Ionut Talos <iulian.talos@p4b.ro>
2020-10-23 18:04:12 +0300

    Create a basic event triggered on key.enter; Update a list on it

--- app/main.js
+++ app/main.js
@@ -1,10 +1,39 @@
 const inputComponent = {
-  template: `<input class="input is-small" type="text" />`
+	template: `<input :placeholder="placeholder" v-model="input" @keyup.enter="monitorEnterKey" class="input is-small" type="text" />`,
+	props: ['placeholder'],
+	data() {
+		return {
+			input: ''
+		}
+	}
+	,
+	methods: {
+		monitorEnterKey() {
+			this.$emit('add-note', {
+				note: this.input,
+				timestamp: new Date().toDateString()
+			});
+
+			this.input = '';
+		}
+	}
 }
 
 new Vue({
-  el: '#app',
-  components: {
-    'input-component': inputComponent
-  }
+	el: '#app',
+	components: {
+		'input-component': inputComponent
+	},
+	data: {
+		notes: [],
+		timestamps: [],
+		placeholder: 'Enter a note'
+	},
+	methods: {
+		addNote(event) {
+			debugger;
+			this.notes.push(event.note);
+			this.timestamps.push(event.timestamp);
+		}
+	}
 })
